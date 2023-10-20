34dbc36305a0eccd6e5b90f822cd8e06bb202417
Maoz Ben Kalif <notreallymyemail@gmail.com>
2023-10-20 14:39:00 +0300

    adding functions to usersBLL

--- BLL/usersBLL.js
+++ BLL/usersBLL.js
@@ -1,17 +1,39 @@
-const usersWS = require("../DAL/usersWS");
+const User = require("../models/userModel");
 
-const getAllUsers = async () => {
-  let { data: users } = await usersWS.getAllUsers();
+/* CRUD - Create , Read, Update, Delete */
 
-  // users = users.map((user) => {
-  //     return {
-  //         id: user.id,
-  //         username: user.username,
-  //         city: user.address.city,
-  //     };
-  // });
-
-  return users;
+//GET -  Get All - Read
+const getAllUsers = () => {
+  return User.find({});
 };
 
-module.exports = { getAllUsers };
+//Get - Get User by ID - Read
+const getUserById = (id) => {
+  return User.findById({ _id: id });
+};
+
+// POST - Create
+const addUser = async (obj) => {
+  const per = new User(obj);
+  await per.save();
+  return "Created! ";
+};
+//PUT - Update
+const updateUser = async (id, obj) => {
+  await User.findByIdAndUpdate(id, obj);
+  return "Users Updated!";
+};
+
+//DELETE - Delete
+const deleteUser = async (id) => {
+  await User.findOneAndDelete({ _id: id });
+  return "User Deleted";
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  addUser,
+  updateUser,
+  deleteUser,
+};
